3b407f0117f5dfca9871075703103c4aee5e4b9b
Laksh <lakshd1999@gmail.com>
2024-10-24 00:05:29 +0530

    changes in design

--- fluxv1/src/Pages/Login.js
+++ fluxv1/src/Pages/Login.js
@@ -1,61 +1,71 @@
-import React, {useState} from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import { UserAuth } from '../context/Authcontext';
-import './Login.css';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/Authcontext";
+import "./Login.css";
 
 const Login = () => {
-  const [email,setEmail] = useState('');
-  const [password, setPassword] = useState('')
-  const [error, SetError] = useState('')
-  const {user, LogIn} = UserAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, SetError] = useState("");
+  const { user, LogIn } = UserAuth();
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) =>{
-    e.preventDefault()
-    SetError('')
-    try{
-      await LogIn(email,password)
-      navigate('/Homepage')
-    }catch(error){
-      console.log(error)
-      SetError(error.message)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    SetError("");
+    try {
+      await LogIn(email, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      SetError(error.message);
     }
-  }
+  };
 
   return (
     <>
+      <div className="Box">
+        <h1 className="log-heading">Log In</h1>
+        <br></br>
+        {error ? <p className="">{error}</p> : null}
 
-<div className='Box'>
-    
-      <h1 className='log-heading'>Log In</h1><br></br>
-      {error?<p className=''>{error}</p>:null}
+        <form onSubmit={handleSubmit} className="">
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            className="tf1"
+            type="email"
+            placeholder="  Enter Your Email"
+          ></input>
+          <br></br>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            className="tf2"
+            type="password"
+            placeholder="  Enter Your Password"
+          />
 
-      <form onSubmit= {handleSubmit}className=''>
-        <input onChange = {(e)=> setEmail(e.target.value)} className='tf1' type="email" placeholder='  Enter Your Email'></input><br></br>
-        <input onChange = {(e)=> setPassword(e.target.value)} className='tf2' type="password" placeholder='  Enter Your Password'/>
-        
+          <div className="">
+            <button className="btn1">Log In To FluxV1</button>
+            <div className="line">
+              <p>
+                <input className="cb1" type="checkbox" />
+                Remeber Me
+              </p>
+              <p className="cb2">Need Help?</p>
+            </div>
+          </div>
 
-        <div className=''>
-        <button className='btn1'>Log In To FluxV1</button>
-        <div className="line">
-        <p><input className='cb1' type="checkbox"/>Remeber Me</p>
-        <p className='cb2'>Need Help?</p>
-        </div>
-        
+          <p className="lastline">
+            <span className="">New To FluxV1?</span>{" "}
+            <Link to="/signup" className="hov">
+              {" "}
+              Sign Up
+            </Link>
+          </p>
+        </form>
       </div>
-
-      <p className='lastline'><span className=''>
-      New To FluxV1?  
-      </span>{' '}
-      <Link to='/signup' className='hov'>     Sign Up</Link>
-      </p>
-      </form>
-      
-    </div>
- 
-
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- fluxv1/src/Pages/Signup.js
+++ fluxv1/src/Pages/Signup.js
@@ -1,54 +1,65 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { UserAuth } from '../context/Authcontext';
-import './Login.css';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/Authcontext";
+import "./Login.css";
 
 const Signup = () => {
-  const [email,setEmail] = useState('');
-  const [password, setPassword] = useState('')
-  const {user, SignUp} = UserAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { user, SignUp } = UserAuth();
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) =>{
-    e.preventDefault()
-    try
-    {
-      await SignUp(email,password)
-      navigate('/Homepage')
-    }catch(error)
-    {
-      console.log(error)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await SignUp(email, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
   return (
     <>
-    <div className='Box'>
+      <div className="Box">
+        <h1 className="log-heading">Sign Up</h1>
+        <br></br>
 
-          <h1 className='log-heading'>Sign Up</h1><br></br>
+        <form onSubmit={handleSubmit} className="">
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            className="tf1"
+            type="email"
+            placeholder="Enter Your Email"
+          ></input>
+          <br></br>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            className="tf2"
+            type="password"
+            placeholder="Create Password"
+          />
+          <button onSubmit={handleSubmit} className="btn1">
+            Sign Up to FluxV1
+          </button>
 
-          <form onSubmit={handleSubmit}  className=''>
-            <input onChange={(e)=> setEmail(e.target.value)} className='tf1' type="email" placeholder='Enter Your Email' ></input><br></br>
-            <input onChange={(e)=> setPassword(e.target.value)} className='tf2' type="password" placeholder='Create Password' />
-            <button onSubmit={handleSubmit} className='btn1'>Sign Up to FluxV1</button>
-
-            <div className='line'>
-          <p><input className='cb1' type="checkbox"/>Remeber Me</p>
-          <p className='cb2'>Need Help?</p> 
+          <div className="line">
+            <p>
+              <input className="cb1" type="checkbox" />
+              Remeber Me
+            </p>
+            <p className="cb2">Need Help?</p>
           </div>
 
-          <p className='lastline'><span className=''>
-          Already Subscribed To FluxV1??
-          </span>{' '}
-          <Link to='/login' className='hov'>Sign In</Link>
+          <p className="lastline">
+            <span className="">Already Subscribed To FluxV1??</span>{" "}
+            <Link to="/login" className="hov">
+              Sign In
+            </Link>
           </p>
-          </form>
-          
-        
-    
-    </div>
+        </form>
+      </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
